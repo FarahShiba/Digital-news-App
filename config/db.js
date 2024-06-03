1818cbbea536dd9eae054a2e9eb6c6219c433ed1
FarahShiba <tambunanfarahshiba@gmail.com>
2024-06-03 22:15:57 +1000

    fix

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const MONGO_URL =
-  process.env.MONGO_URL ||
+  process.env.NEXT_PUBLIC_MONGO_URL ||
   "mongodb+srv://tambunanfarahshiba:fgMEvxKXZyzELAVX@courses.dbex1kq.mongodb.net/digital-news";
 
 if (!MONGO_URL) {
