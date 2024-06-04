7adf28a67b31646abdded318f8a825da81eb3403
FarahShiba <tambunanfarahshiba@gmail.com>
2024-06-04 16:36:28 +1000

    fixing

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,9 +1,29 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
+
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {GA_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics-script" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_ID}');
+              `}
+            </Script>
+          </>
+        )}
+      </Head>
       <body>
         <Main />
         <NextScript />
