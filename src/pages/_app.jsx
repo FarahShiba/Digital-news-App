c01792c66b32c18f42e3d04da107c40c5a1c0be2
FarahShiba <tambunanfarahshiba@gmail.com>
2024-06-05 17:56:11 +1000

    fixing

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,6 @@
 import dynamic from "next/dynamic";
+import Script from "next/script";
+import Head from "next/head";
 import "bootstrap/dist/css/bootstrap.css";
 import "react-bootstrap-icons";
 import "aos/dist/aos.css";
@@ -6,6 +8,7 @@ import { EB_Garamond } from "next/font/google";
 import "../styles/variables.css";
 import "../styles/globals.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 const ebGaramond = EB_Garamond({
   subsets: ["latin"],
@@ -19,6 +22,8 @@ const Footer = dynamic(() => import("../components/layout/footer/Footer"), {
   ssr: false,
 });
 
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
@@ -27,6 +32,25 @@ function MyApp({ Component, pageProps }) {
           font-family: ${ebGaramond.className};
         }
       `}</style>
+      <Head>
+        {GA_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics-script" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${GA_ID}');
+              `}
+            </Script>
+          </>
+        )}
+      </Head>
+      <GoogleAnalytics trackPageViews />
       <Header />
       <Component {...pageProps} />
       <Footer />
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,32 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
-
-const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export default function Document() {
-  console.log("Google Analytics ID:", GA_ID);
   return (
     <Html lang="en">
-      <Head>
-        {GA_ID && (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-              strategy="afterInteractive"
-            />
-            <Script id="google-analytics-script" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                console.log('dataLayer initialized:', window.dataLayer);
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_ID}');
-                console.log('gtag config called with ID:', '${GA_ID}');
-              `}
-            </Script>
-          </>
-        )}
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
